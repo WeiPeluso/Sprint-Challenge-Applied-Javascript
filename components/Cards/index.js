dc0dda1c9da0bd5bb7b056b23009def8a6525870
wei peluso <weipeluso@gmail.com>
2020-05-08 13:21:08 -0400

    finished mvp and stretch

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,56 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function cardCreator(artical){
+    const{headerline:headerline,author:authorName,imgUrl:url}=artical
+
+const card=document.createElement('div')
+const headerLine=document.createElement('div')
+const author=document.createElement('div')
+const imgContainer=document.createElement('div')
+const image=document.createElement('img')
+const spanBy=document.createElement('span')
+
+//add class
+card.classList.add('card')
+headerLine.classList.add('headline')
+author.classList.add('author')
+imgContainer.classList.add('img-container')
+
+card.appendChild(headerLine);
+headerLine.textContent=headerline
+card.appendChild(author)
+author.appendChild(imgContainer)
+imgContainer.appendChild(image)
+image.src=url
+author.appendChild(spanBy)
+spanBy.textContent=`By ${authorName}`
+
+return card
+
+}
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+.then((lambda)=>{
+  // console.log(lambda)
+   for (const topic in lambda.data.articles) {
+      //console.log(topic)
+      //console.log(lambda.data.articles[topic])
+
+      lambda.data.articles[topic].forEach((article) => {
+       document.querySelector('.cards-container').appendChild(
+           cardCreator({headerline:`${article.headline}`,
+            author:`${article.authorName}`,
+            imgUrl: `${article.authorPhoto}`})//end of create card
+       )//end of appendchild
+
+   })// end of for each
+  }// end of for
+})//end of then
+.catch((error)=>{
+    console.log('failed');
+})
+.finally(()=>{
+    console.log('done');
+})
--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,34 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function headerCreater() {
+
+    const header=document.createElement('div')
+    const spanDate=document.createElement('span')
+    const h1=document.createElement('h1')
+    const spanTem=document.createElement('span')
+
+    //class
+    header.classList.add('header')
+    spanDate.classList.add('date')
+    spanTem.classList.add('temp')
+
+    header.appendChild(spanDate)
+    header.appendChild(h1)
+    header.appendChild(spanTem)
+   
+    //set content
+
+    spanDate.textContent="MARCH 28, 2019"
+    h1.textContent="Lambda Times"
+    spanTem.textContent="98°"
+
+    return header;
+
+}
+
+//console.log(headerCreater())
+
+document.querySelector('.header-container')
+.appendChild(headerCreater())
+
